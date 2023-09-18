b160cc61dff213fc4ca8f9d884e093e7349d1245
Tycho <1756518455@qq.com>
2023-09-18 21:32:04 +0800

    update event-driven

--- event-driven/eventEmit.js
+++ event-driven/eventEmit.js
@@ -1,32 +1,52 @@
-class eventEmit {
+class EventEmitter {
     constructor() {
-        this.event = {};
+        this.events = {};
     }
 
+    // 订阅事件
     on(eventName, callback) {
-        this.event[eventName] = this.event[eventName] || [];
-        this.event[eventName].push(callback);
+        if (!this.events[eventName]) {
+            this.events[eventName] = [];
+        }
+        this.events[eventName].push(callback);
     }
 
+    // 发布事件
     emit(eventName, data) {
-        if(this.event[eventName]) {
-            this.event[eventName].forEach( callback => {
+        const callbacks = this.events[eventName];
+        if (callbacks) {
+            for (const callback of callbacks) {
                 callback(data);
-            })
+            }
         }
     }
 
+    // 取消订阅事件
     off(eventName, callback) {
-        if(this.event[eventName]) {
-            this.event[eventName] = this.event[eventName].filter( cb => cb !== callback);
+        const callbacks = this.events[eventName];
+        if (callbacks) {
+            this.events[eventName] = callbacks.filter(cb => cb !== callback);
         }
     }
 
+    // 一次性订阅事件
     once(eventName, callback) {
         const onceCallback = data => {
             callback(data);
             this.off(eventName, onceCallback);
-        }
+        };
         this.on(eventName, onceCallback);
     }
 }
+
+// 示例用法：
+const emitter = new EventEmitter();
+
+const logHello = () => console.log("Hello!");
+const logWorld = () => console.log("World!");
+
+emitter.on("greet", logHello);
+emitter.once("greet", logWorld);
+
+emitter.emit("greet", null); // 输出 "Hello!" 和 "World!"
+emitter.emit("greet", null); // 只输出 "Hello!"
